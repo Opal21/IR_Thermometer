f4425c1b54636a3b1e054805b5bdfa35a7efd350
Marcin Opaliński <marcinopalinskito@gmail.com>
2022-10-22 16:56:27 +0200

    Remove unnecessary files; Implement full functionality;

--- IR_Thermometer.ino
+++ IR_Thermometer.ino
@@ -1,62 +1,135 @@
-#include <millisDelay.h>
-#include <Wire.h> 
 #include <Adafruit_SSD1306.h>
+#include <Adafruit_MLX90614.h>
+#include <Wire.h>
+#include <millisDelay.h>
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_WIDTH 128 // OLED display width [px]
+#define SCREEN_HEIGHT 64 // OLED display height [px]
 
-const int statePin = 9;     // pin D9 for IR proximity sensor
-const int RED_LED = 5;      // pin D5 for red LED
-const unsigned long delay_hold_red = 5000; // delay hold red LED in mS
+const int PROXIMITY_SENSOR = 9; // pin D9 for IR proximity sensor
+const int RED_LED = 5;          // pin D5 for red LED
 
-const unsigned long interval_display = 500; // interval refresh OLED display in mS 
-millisDelay displayDelay;                   // the delay object
+bool isTempMeasured = false; 
 
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+const unsigned long delay_hold_red = 5000; // LED delay hold [ms]
+millisDelay holdDelay;  
+
+const unsigned long interval_sensor = 50; // interval refresh sensor [mS] 
+millisDelay sensorDelay;
+
+const unsigned long interval_display = 500; // interval refresh display [mS] 
+millisDelay displayDelay;
+
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
 void setup() {
     Serial.begin(9600);
 
     pinMode(RED_LED, OUTPUT);
-    pinMode(statePin, INPUT);
+    pinMode(PROXIMITY_SENSOR, INPUT);
   
-    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
-        Serial.println(F("OLED Display allocation failed"));
+    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+        Serial.println(F("OLED Display allocation failed")); // Address 0x3D for 128x64
         for(;;);
     }
 
     display.clearDisplay();
-    display.setTextSize(1);
+    display.setTextSize(2);
     display.setTextColor(WHITE);
     display.setCursor(20, 20);
     display.println("Initializing");
     display.display();
-    delay(2500);
+    delay(1500);
     display.clearDisplay();
-  
-    displayDelay.start(interval_display); //refresh display
+    displayDelay.start(interval_display);
+
+    mlx.begin();
 }
 
 void loop() {
-    int state = digitalRead(statePin);  //IR sensor condition
+    static float temp = -1;
+    int state = digitalRead(PROXIMITY_SENSOR);
 
-    if (state == LOW) {
-    digitalWrite(RED_LED, HIGH);
+    if (state == LOW && isTempMeasured == false) {
+        digitalWrite(RED_LED, HIGH);
+        sensorDelay.start(interval_sensor);
+        displayDelay.finish();
+        isTempMeasured = true;
+    } else if (isTempMeasured == true) {
+        temp = getTemp();
+    } else {
+        temp = -1;
+    }
+    
+    showTemp(temp);
+    holdReading();
+}
+
+float getTemp() {
+    static int i = 0;
+    static float tempSum = 0;
+    float result = 0;
+
+    if (sensorDelay.justFinished()) {
+        sensorDelay.repeat();
+        tempSum += mlx.readObjectTempC();
+        i++;
+    }
+
+    if(i == 19) {
+        result = tempSum / 20;
+        tempSum = 0;
+        i = 0;
+        sensorDelay.stop(); 
+        displayDelay.finish();
+    }
+
+  return result;
+}
+
+void showTemp(float temperature) {
+    if (displayDelay.justFinished()) {
+        displayDelay.repeat();
+
+        if(temperature == -1) { // if there are no object in front of sensor
+            display.clearDisplay();
+            display.setTextSize(2);
+            display.setCursor(35, 5);
+            display.print("-----");
+            display.setCursor(105, 20);
+            display.print("");
+            display.setTextSize(2);
+            display.setCursor(35, 40);
+            display.print("-----");
+            display.setCursor(105, 46);
+            display.print("");
+            display.display();
+        } else if(temperature == 0) { // if still reading temperature
+            display.clearDisplay();
+            display.setTextSize(1);
+            display.setTextColor(WHITE);
+            display.setCursor(20, 25);
+            display.println("WAIT ....");
+            display.display();
+        } else { // if there is a new result
+            temperature += 8.5;
+            display.clearDisplay();     
+            display.setTextSize(3);
+            display.setCursor(10, 20);
+            display.print(temperature,1);      
+            display.print(" C");
+            display.display();
+            displayDelay.stop();
+        }
+    }
+}
+
+void  holdReading() {
+    if (holdDelay.justFinished()) {
+        digitalWrite(RED_LED, LOW);
+        isTempMeasured = false;
+        displayDelay.start(interval_display);
   }
-
-    display.clearDisplay();
-    display.setTextSize(2);
-    display.setCursor(35, 5);
-    display.print("-----");
-    display.setCursor(105, 20);
-    display.print("");
-    display.setTextSize(2);
-    display.setCursor(35, 40);
-    display.print("-----");
-    display.setCursor(105, 46);
-    display.print("");
-    display.display();
-    delay(25000);
-    displayDelay.stop(); //stop refresh display
 }
